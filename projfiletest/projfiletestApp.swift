3dd57784a60db326f0b84fbb4323a223d84fbe78
TLtlbo <js1436kt@gmail.com>
2024-08-03 16:00:02 +0900

    Initial Commit

---
+++ projfiletest/projfiletestApp.swift
@@ -0,0 +1,17 @@
+//
+//  projfiletestApp.swift
+//  projfiletest
+//
+//  Created by 박진성 on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct projfiletestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
